9a69c75283e9075080605308451a05dcb301a5c8
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-04-20 11:50:27 +0100

    cleaning up

--- score_clustering/cluster.py
+++ score_clustering/cluster.py
@@ -23,20 +23,12 @@ class Cluster:
             for neighbor in point.neighbors:
                 if neighbor in self.points:
                     self._graph.add_edge(point, neighbor)
-        #self.connected_components = [self._graph.subgraph(c).copy() for c in nx.connected_components(self._graph)]
-        #self.articulation_points = [nx.articulation_points(c) for c in self.connected_components]
-        self.articulation_points = nx.articulation_points(self._graph)
         for point in points:
             point.cluster = self
+        self.articulation_points = nx.articulation_points(self._graph)
 
     def is_articulation_point(self, point):
         return point in self.articulation_points
-        #for i, c in enumerate(self.connected_components):
-        #    if point in c:
-        #        return point in self.articulation_points[i]
-        #connected_component = self._graph.subgraph(nx.node_connected_component(self._graph, point))
-        #ret = point in nx.articulation_points(connected_component)
-        #return ret
 
     def add(self, point):
         self._graph.add_node(point)
@@ -48,16 +40,12 @@ class Cluster:
         self.points.add(point)
         point.cluster = self
         self.articulation_points = nx.articulation_points(self._graph)
-        #self.connected_components = [self._graph.subgraph(c).copy() for c in nx.connected_components(self._graph)]
-        #self.articulation_points = [nx.articulation_points(c) for c in self.connected_components]
 
     def remove(self, point):
         self._graph.remove_node(point)
         self.points.remove(point)
         point.cluster = None
         self.articulation_points = nx.articulation_points(self._graph)
-        #self.connected_components = [self._graph.subgraph(c).copy() for c in nx.connected_components(self._graph)]
-        #self.articulation_points = [nx.articulation_points(c) for c in self.connected_components]
 
     def __getitem__(self, index):
         return self.points[index]
